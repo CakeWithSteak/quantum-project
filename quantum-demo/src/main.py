a1d93e8646db3d094c547ae2ec4ce1810e292c88
MarkasAis <mark.aisparas@gmail.com>
2024-01-17 20:49:54 +0100

    Simulating 2d rules

--- quantum-demo/src/main.py
+++ quantum-demo/src/main.py
@@ -1,22 +1,34 @@
+from qiskit import QuantumCircuit
 
-from qiskit import QuantumCircuit, Aer, execute
-from src.utils import make_grid_partitions, make_truth_table_update, apply_global_update
+from src.simulate import simulate
+from src.utils import make_grid_partitions, make_truth_table_update, apply_global_update, init
+from src.visualize import visualize_grid
 
-partitions_A = make_grid_partitions(4, 2, 2, 1, 0, 0, True)
-partitions_B = make_grid_partitions(4, 2, 2, 1, 1, 0, True)
+partitions_A = make_grid_partitions(4, 4, 2, 2, 0, 0, False)
+partitions_B = make_grid_partitions(4, 4, 2, 2, 0, 1, False)
+partitions_C = make_grid_partitions(4, 4, 2, 2, 1, 0, False)
+partitions_D = make_grid_partitions(4, 4, 2, 2, 1, 1, False)
 
-local_update = make_truth_table_update(['01', '10', '11', '00'])
+local_update = make_truth_table_update([
+    '0000', '1000', '0100', '1100',
+    '0010', '1010', '1001', '1110',
+    '0001', '0110', '0101', '1101',
+    '0011', '1011', '0111', '1111'])
 
-circuit = QuantumCircuit(8)
-apply_global_update(circuit, partitions_A, local_update)
-apply_global_update(circuit, partitions_B, local_update)
+p = [partitions_A, partitions_B, partitions_C, partitions_D]
 
-circuit.measure_all()
-print(circuit.draw())
+for i in range(10):
+    circuit = QuantumCircuit(16)
 
-# simulate
-simulator = Aer.get_backend('qasm_simulator')
-job = execute(circuit, simulator, shots=1000)
-result = job.result()
-counts = result.get_counts(circuit)
-print(counts)
+    init(circuit,
+         '1000'
+         '0000'
+         '0000'
+         '0000')
+
+    for j in range(i):
+        apply_global_update(circuit, p[j % len(p)], local_update)
+
+    circuit.measure_all()
+    counts = simulate(circuit)
+    visualize_grid(4, 4, counts)
---
+++ quantum-demo/src/simulate.py
@@ -0,0 +1,9 @@
+from qiskit import Aer, execute
+
+
+def simulate(circuit):
+    simulator = Aer.get_backend('qasm_simulator')
+    job = execute(circuit, simulator, shots=1000)
+    result = job.result()
+    counts = result.get_counts(circuit)
+    return counts
