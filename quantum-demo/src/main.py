5e8bc2bb22048632d76ddfa14dfb51333ca8d493
MarkasAis <mark.aisparas@gmail.com>
2024-01-17 19:47:23 +0100

    Added looping grid

--- quantum-demo/src/main.py
+++ quantum-demo/src/main.py
@@ -2,14 +2,14 @@
 from qiskit import QuantumCircuit, Aer, execute
 from src.utils import make_grid_partitions, make_truth_table_update, apply_global_update
 
-partitions = make_grid_partitions(2, 2, 2, 1, 0, 0)
-print(partitions)
+partitions_A = make_grid_partitions(4, 2, 2, 1, 0, 0, True)
+partitions_B = make_grid_partitions(4, 2, 2, 1, 1, 0, True)
 
 local_update = make_truth_table_update(['01', '10', '11', '00'])
 
-circuit = QuantumCircuit(4)
-apply_global_update(circuit, partitions, local_update)
-apply_global_update(circuit, partitions, local_update)
+circuit = QuantumCircuit(8)
+apply_global_update(circuit, partitions_A, local_update)
+apply_global_update(circuit, partitions_B, local_update)
 
 circuit.measure_all()
 print(circuit.draw())
--- quantum-demo/src/utils.py
+++ quantum-demo/src/utils.py
@@ -12,78 +12,54 @@ def make_truth_table_matrix(bitmaps):
     return binary_mapping_matrix
 
 
-def make_basis(restrictions):
-    basis = []
-    bits_n = len(restrictions)
-
-    for input_index in range(2 ** bits_n):
-        valid = True
-        for bit_index in range(bits_n):
-            restriction = restrictions[bit_index]
-            if restriction != -1 and not bit_equal(input_index, bit_index, restriction):
-                valid = False
-                break
-
-        if valid:
-            basis.append(input_index)
-
-    return basis
-
-
-def compress_matrix(matrix, basis):
-    n = len(basis)
-    compressed = np.zeros((n, n), dtype=int)
-    for i in range(n):
-        for j in range(n):
-            compressed[i, j] = matrix[basis[i], basis[j]]
-
-    return compressed
-
-
 def make_truth_table_update(bitmaps):
     matrix = make_truth_table_matrix(bitmaps)
 
-    def local_update(circuit, qubits, outside=0):
-        restrictions = [outside if qubits[i] == -1 else -1 for i in range(len(qubits))]
-        basis = make_basis(restrictions)
-
-        compressed_matrix = compress_matrix(matrix, basis)
-        compressed_qubits = [q for q in qubits if q != -1]
-        circuit.unitary(compressed_matrix, compressed_qubits)
+    def local_update(circuit, qubits):
+        circuit.unitary(matrix, qubits)
 
     return local_update
 
 
-def bit_equal(number, index, target):
-    return (number >> index) & 1 == target
-
-
-def get_grid_index(grid_width, grid_height, x, y):
-    if x < 0 or x >= grid_width or y < 0 or y >= grid_height:
+def get_grid_index(grid_width, grid_height, x, y, loop):
+    if loop:
+        x %= grid_width
+        y %= grid_height
+    elif x < 0 or x >= grid_width or y < 0 or y >= grid_height:
         return -1
 
     return y*grid_width + x
 
 
-def make_grid_partitions(grid_width, grid_height, partition_width, partition_height, shift_x, shift_y):
+def make_grid_partitions(grid_width, grid_height, partition_width, partition_height, shift_x, shift_y, loop=True):
     partitions = []
 
-    start_x = shift_x % -partition_width
-    start_y = shift_y % -partition_height
+    start_x = shift_x % partition_width
+    start_y = shift_y % partition_height
 
-    for y in range(start_y, grid_height, partition_height):
-        for x in range(start_x, grid_width, partition_width):
+    if loop:
+        assert grid_width % partition_width == 0 and grid_height % partition_height == 0
+        partition_count_x = grid_width // partition_width
+        partition_count_y = grid_height // partition_height
+    else:
+        partition_count_x = (grid_width-start_x) // partition_width
+        partition_count_y = (grid_height-start_y) // partition_height
+
+    for py in range(0, partition_count_y):
+        for px in range(0, partition_count_x):
+            sx = start_x + px*partition_width
+            sy = start_y + py*partition_height
             partition = []
 
-            for sy in range(y, y + partition_height):
-                for sx in range(x, x+partition_width):
-                    partition.append(get_grid_index(grid_width, grid_height, sx, sy))
+            for y in range(sy, sy+partition_height):
+                for x in range(sx, sx+partition_width):
+                    partition.append(get_grid_index(grid_width, grid_height, x, y, loop))
 
             partitions.append(partition)
 
     return partitions
 
 
-def apply_global_update(circuit, partitions, local_update, outside=0):
+def apply_global_update(circuit, partitions, local_update):
     for qubits in partitions:
-        local_update(circuit, qubits, outside)
+        local_update(circuit, qubits)
